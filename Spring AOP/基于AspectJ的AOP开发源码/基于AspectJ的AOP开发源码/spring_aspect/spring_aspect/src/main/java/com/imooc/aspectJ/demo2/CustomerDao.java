6f87014e3b1150c49b75e31bc799c0cf2e9e54b4
wang <525766003@qq.com>
2019-01-16 12:13:48 +0800

    删除了AOP

--- Spring AOP/基于AspectJ的AOP开发源码/基于AspectJ的AOP开发源码/spring_aspect/spring_aspect/src/main/java/com/imooc/aspectJ/demo2/CustomerDao.java
+++
@@ -1,9 +0,0 @@
-package com.imooc.aspectJ.demo2;
-
-public interface CustomerDao {
-    public void save();
-    public String update();
-    public void delete();
-    public void findOne();
-    public void findAll();
-}
